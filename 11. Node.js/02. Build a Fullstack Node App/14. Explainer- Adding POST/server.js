b1cb67230dcb930b48d04a6eb1d26ebf896454f3
Satyam Kumar <189542961+ImSatyamK@users.noreply.github.com>
2026-03-22 20:32:12 +0530

    Add POST request handling to the server

--- 11. Node.js/02. Build a Fullstack Node App/14. Explainer- Adding POST/server.js
+++ 11. Node.js/02. Build a Fullstack Node App/14. Explainer- Adding POST/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
 import { handleGet } from './handlers/routeHandlers.js'
+import { handlePost } from './handlers/routeHandlers.js'
 
 const PORT = 8000
 
@@ -12,11 +13,9 @@ const server = http.createServer(async (req, res) => {
         if (req.method === 'GET') {
             return await handleGet(res)
         }
-/*
-Challenge: 
-   1. Add a route for a POST request to '/api'.
-   2. When a request comes in, pass the req and res to handlePost().
-*/
+        else if (req.method === 'POST') {
+            return await handlePost(req, res)
+        }
     }
     else if (!req.url.startsWith('/api')) {
         return await serveStatic(req, res, __dirname)
